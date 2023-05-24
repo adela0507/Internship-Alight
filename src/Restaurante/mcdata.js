f57d499ae84f12e25311fff772565c4f22202b58
adela miclea <adelamiclea709@yahoo.com>
2023-05-25 00:46:59 +0300

    partea1

---
+++ src/Restaurante/mcdata.js
@@ -0,0 +1,28 @@
+const mc=[
+    {
+        id:1,
+        image:'https://www.mcdonalds.ro/sites/default/files/styles/500x500/public/field_product_image/2022-08/500p-2.png?itok=zh6vahQP',
+        name:'Crispy Chicken Tenders',
+        descriprion:'3 Bucăți gustoase din piept de pui fraged, cu înveliș crocant de porumb și piper, facute din pui 100% românesc.',
+        price:'',
+    },
+    {
+        id:2,
+        image:'https://www.mcdonalds.ro/sites/default/files/styles/500x500/public/field_product_image/2021-03/McChicken.png?itok=d3RF6nf9',
+        name:'McChicken™',
+        descriprion:'O felie de carne de piept de pui într-un strat crocant de pesmet auriu, însoţită de salată verde, sos McChicken și chiflă cu susan.',
+    },
+    {
+        id:3,
+        image:'https://www.mcdonalds.ro/sites/default/files/styles/500x500/public/field_product_image/2021-03/Mc_Puisor.png?itok=aaKzVOZG',
+        name:'McPuişor™',
+        descriprion:'O felie de carne de pui într-un strat crocant de pesmet auriu, însoţită de sos McPuișor, castraveţi muraţi și chiflă.',
+    },{
+        id:4,
+        image:'https://www.mcdonalds.ro/sites/default/files/styles/500x500/public/field_product_image/2021-03/Placinta_visine.png?itok=lx-n07Lz',
+        name:'Plăcintă cu vişine',
+        descriprion:'Un desert fierbinte şi deosebit cu vişine, învelit într-o crustă de foietaj.',
+    }
+    
+];
+export default mc;
